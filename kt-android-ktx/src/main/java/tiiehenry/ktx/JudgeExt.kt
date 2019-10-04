6a7c4ab381eefbec27158a99373271e088993aa6
TIIEHenry <1806968131@qq.com>
2019-10-04 16:06:09 +0800

    add JudgeExt

---
+++ kt-android-ktx/src/main/java/tiiehenry/ktx/JudgeExt.kt
@@ -0,0 +1,27 @@
+package tiiehenry.ktx
+
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+fun Boolean.judge(f: (String) -> Unit) {
+
+}
+
+public inline fun <T, R> T.judge(block: (T) -> R): R {
+    return block(this)
+}
+
+fun <Boolean, R> Boolean.ifFalse(block: (Boolean) -> R) {
+    if (this == false)
+        block.invoke(this)
+}
+
+fun <Boolean, R> Boolean.ifTrue(block: (Boolean) -> R) {
+    if (this == false)
+        block.invoke(this)
+}
+
+inline fun Any?.ifNull(block: () -> Unit) {
+    if (this == null)
+        block.invoke()
+}
